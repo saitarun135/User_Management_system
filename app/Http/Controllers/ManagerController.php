2ca7dc84d77b967bb11d6f0ec102b536726e8886
saitarun135 <saitarun800@gmail.com>
2022-09-22 13:25:27 +0530

    session implemented

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -13,24 +13,35 @@ class ManagerController extends Controller
         $this->repository = $repository;
     }
 
-    public function register(ManagerRequest $request){
+    public function register(ManagerRequest $request)
+    {
         $data = $request->all();
         $data['password'] = $this->binaryPassword($data['password']);
         $this->repository->register($data);
         return redirect('login');
     }
 
-    public function login(ManagerRequest $request){
+    public function login(ManagerRequest $request)
+    {
         $data = $request->all();
         $data['password'] = $this->binaryPassword($data['password']);
         $result = $this->repository->login($data);
-        if(count($result) < 1){
-           return  redirect()->back()->withErrors(['count'=>'Invalid credentials']);
+        if (count($result) < 1) {
+            return redirect()->back()->withErrors(['count' => 'Invalid credentials']);
+        }
+        if (isset($result[0])) {
+            session(['user' => [$result[0]]]);
         }
         return redirect('dashboard');
     }
 
-    public function binaryPassword($pass){
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function binaryPassword($pass)
+    {
         return $this->repository->model()::getPasswordAttribute($pass);
     }
 }
