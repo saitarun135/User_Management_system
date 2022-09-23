ba450129247201e3295fc823a38772b8d4321f56
saitarun135 <saitarun800@gmail.com>
2022-09-23 10:55:52 +0530

    date format value handeled

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Employeerequest;
 use App\Models\Employee;
 use App\Repositories\EmployeeRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -16,7 +17,12 @@ class EmployeeController extends Controller
     }
 
     public function create(Employeerequest $request){
-        $this->repository->postEmployee($request->all(),$this->getLoggedInID());
+        $data = $request->all();
+        if(isset($data))
+        {
+            $data['current_date']=date(Carbon::now());
+        }
+        $this->repository->postEmployee($data,$this->getLoggedInID());
         redirect('dashboard');
     }
 }
