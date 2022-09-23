a6881586d76cecd22776f196c6c2a2bddfab9e02
saitarun135 <saitarun800@gmail.com>
2022-09-23 17:34:31 +0530

    Experience Date Formats are refcatored

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -35,15 +35,25 @@ class EmployeeController extends Controller
 
     public function dashboard()
     {
-        $this->repository->pushCriteria(new WhereCriteria('manager_id', $this->getLoggedInID()));
-        $employees = $this->repository->all();
+        $employees = $this->repository->with('managers')->all();
         $employees->filter(function ($items) {
             $split_date = date('Y-m-d', strtotime($items['joining_date']));
             $date = explode('-', $split_date);
             if (count($date) == 3) {
                 isset($items['date_of_leaving']) ? $todate= $items['date_of_leaving'] : $todate = Carbon::now();
                 $joining_date = Carbon::create($date[0], $date[1], $date[2]);
-                $experience = $joining_date->diff($todate)->format('%y Years, %m Months and %d Days');
+                $experience = $joining_date->diff($todate);
+                switch($experience){
+                    case ($experience->format('%y')==0):
+                        $experience = $experience->format('%d Days');
+                        break;
+                    case ($experience->format('%m')==0):
+                        $experience = $experience->format('%d Days');
+                        break;
+                    default:
+                    $experience = $experience->format('%y Year, %m Months and %d Days');
+                    break;
+                }
                 $items['joining_date'] = $experience;
             }
         });
