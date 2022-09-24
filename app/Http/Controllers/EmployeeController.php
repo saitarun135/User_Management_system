58f1bbe3d113a38904b40e05c273e922a6728abb
saitarun135 <saitarun800@gmail.com>
2022-09-24 11:04:57 +0530

    order by for dashboard data

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -33,7 +33,7 @@ class EmployeeController extends Controller
 
     public function dashboard()
     {
-        $employees = $this->repository->with('managers')->all();
+        $employees = $this->repository->with('managers')->orderBy('id','DESC')->all();
         $employees->filter(function ($items) {
             $split_date = date('Y-m-d', strtotime($items['joining_date']));
             $date = explode('-', $split_date);
